dda4097cba894d2493fd187d6ae02a482ad47b43
jonot-cyber <joetgamerman3@gmail.com>
2020-02-17 13:06:29 -0500

    added tileset

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var direction      : Vector2 = Vector2(0,0)
 
-export var speed   : float = 5
+export var speed   : float = 10
 export var gravity : float = .1
 
 onready var camera : Camera2D = get_node("Camera")
@@ -18,15 +18,13 @@ func _process(_delta):
 			direction.x = get_local_mouse_position().normalized().x * speed
 			direction.y = get_local_mouse_position().normalized().y * speed
 	direction.y += gravity
-<<<<<<< HEAD
+
 	direction.y = clamp(direction.y,0,5)
 	if direction.x < 0:
 		$Sprite.set_flip_h(true)
 	elif direction.x > 0:
 		$Sprite.set_flip_h(false)
-=======
-	direction.y = clamp(direction.y,-5,5)
->>>>>>> 8876eb86795b9756c468185c92db5897146935ff
+
 	
 func _physics_process(_delta):
 	move_and_collide(direction * speed)
@@ -36,8 +34,8 @@ func reset():
 	position = get_node("../Spawn").position
 
 func _input(event):
-    if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-        if not dragState:
-            dragState = true
-        else:
-            dragState = false
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if not dragState:
+			dragState = true
+		else:
+			dragState = false
