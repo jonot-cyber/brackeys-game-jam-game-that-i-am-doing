b576935fe10d8517e4f10eea49c44ef09b9fc178
LeoDog896 <LeoDog896@hotmail.com>
2020-02-16 20:56:23 -0500

    Push cool movement

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,22 +2,35 @@ extends KinematicBody2D
 
 var direction = Vector2(0,0)
 
-export var speed = 5
-export var gravity = .1
+export var speed   : float = 5
+export var gravity : float = .1
 
 onready var camera = get_node("Camera")
 
+var mousePosition
+var dragState
+
 func _ready():
+	reset()
 	camera.current = true
 
 func _process(_delta):
-	direction.x = 0
-	if Input.is_key_pressed(KEY_A):
-		direction.x -= 1
-	elif Input.is_key_pressed(KEY_D):
-		direction.x += 1
+	if dragState and direction.y == 5:
+			direction.x = get_local_mouse_position().normalized().x * speed
+			direction.y = get_local_mouse_position().normalized().y * speed
 	direction.y += gravity
-	direction.y = clamp(direction.y,0,5)
+	direction.y = clamp(direction.y,-5,5)
 	
 func _physics_process(_delta):
 	move_and_collide(direction * speed)
+
+func reset():
+	direction = Vector2(0,0)
+	position = get_node("../Spawn").position
+
+func _input(event):
+    if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+        if not dragState:
+            dragState = true
+        else:
+            dragState = false
