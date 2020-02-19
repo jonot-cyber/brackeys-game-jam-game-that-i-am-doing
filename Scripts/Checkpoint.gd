68a8bf4cd246d1cb0051f1b986cd76624abb8f47
jonot-cyber <joetgamerman3@gmail.com>
2020-02-19 18:47:57 -0500

    color changes on checkponit

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -1,9 +1,11 @@
 extends Position2D
 
-var foo : String = "bar"
+export var color : Color
+
 signal checkpoint
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		get_node("../Spawn").position = position
+		get_node("../Player/Background").modulate = color
 		emit_signal("checkpoint")
