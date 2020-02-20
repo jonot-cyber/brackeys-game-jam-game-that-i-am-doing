05ab0d4138552736071d6b6d7f0b38cac361ec08
LeoDog896 <LeoDog896@hotmail.com>
2020-02-19 19:52:15 -0500

    org

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -3,16 +3,21 @@ extends Position2D
 export var color : Color
 export var duration : float = 1
 
-onready var tween : Tween = get_node("Tween")
-onready var background : Sprite = get_node("../Player/Background")
+onready var tween      : Tween =           get_node("Tween")
+onready var background : Sprite =          get_node("../Player/Background")
+onready var spawn      : Position2D =      get_node("../Spawn")
+onready var player     : KinematicBody2D = get_node("../Player")
+
+const TRANS = Tween.TRANS_LINEAR
+const EASE = Tween.EASE_IN_OUT
 
 signal checkpoint
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
-		get_node("../Spawn").position = position
-		get_node("../Player")._on_checkpoint()
+		spawn.position = position
+		player._on_checkpoint()
 		print(modulate)
-		tween.interpolate_property(background, "modulate", background.modulate, color, duration, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+		tween.interpolate_property(background, "modulate", background.modulate, color, duration, TRANS, EASE)
 		tween.start()
 		emit_signal("checkpoint")
--- Scripts/CheckpointText.gd
+++ Scripts/CheckpointText.gd
@@ -1,15 +1,17 @@
 extends Label
 
-onready var tween : Tween = get_node("Tween")
+onready var tween    : Tween = get_node("Tween")
+onready var textfade : Timer = get_node("TextFade")
 
 func _on_checkpoint():
 	visible = true
 	rect_position = Vector2(-1532,-896)
+	
 	tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
 	tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
 	tween.start()
 	
-	$TextFade.start()
+	textfade.start()
 	
 func _on_TextFade_timeout():
 	visible = false
