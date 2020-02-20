a9daf9b1939cf05cc2cd085a52d973cef13f664b
LeoDog896 <LeoDog896@hotmail.com>
2020-02-19 19:45:58 -0500

    tween color

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -2,10 +2,15 @@ extends Position2D
 
 export var color : Color
 
+onready var tween : Tween = get_node("Tween")
+onready var background : Sprite = get_node("../Player/Background")
 signal checkpoint
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		get_node("../Spawn").position = position
-		get_node("../Player/Background").modulate = color
+		get_node("../Player")._on_checkpoint()
+		print(modulate)
+		tween.interpolate_property(background, "modulate", background.modulate, color, 1, Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+		tween.start()
 		emit_signal("checkpoint")
--- Scripts/CheckpointText.gd
+++ Scripts/CheckpointText.gd
@@ -1,11 +1,14 @@
 extends Label
 
+onready var tween : Tween = get_node("Tween")
+
 func _on_checkpoint():
 	visible = true
 	rect_position = Vector2(-1532,-896)
-	$Tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
-	$Tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
-	$Tween.start()
+	tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
+	tween.interpolate_property(self,"rect_position",rect_position,Vector2(rect_position.x,-2000),1,Tween.TRANS_QUAD,Tween.EASE_IN)
+	tween.start()
+	
 	$TextFade.start()
 	
 func _on_TextFade_timeout():
