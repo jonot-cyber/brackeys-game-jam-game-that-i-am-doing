882a9db1bcfb154d84b4e0fcf5edc00f114f45b9
LeoDog896 <LeoDog896@hotmail.com>
2020-02-20 20:12:14 -0500

    quick changeos

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,10 +1,4 @@
 extends Button
 
 func press():
-	print(text)
-	if name == "Play":
-		get_tree().change_scene("res://Scenes/Game.tscn")
-	elif name == "Credits":
-		get_tree().change_scene("res://Scenes/Credits.tscn")
-	elif name == "Instructions":
-		get_tree().change_scene("res://Scenes/Instructions.tscn")
+	get_tree().change_scene("res://Scenes/" + name + ".tscn")
