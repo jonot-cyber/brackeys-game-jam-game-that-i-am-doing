ef82e3219714cbb829669fc1b211f0ef2de59ff4
LeoDog896 <LeoDog896@hotmail.com>
2020-02-20 19:22:58 -0500

    added levels

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -5,9 +5,9 @@ export var duration : float = 1
 export var switch : String = "red"
 
 onready var tween      : Tween =           get_node("Tween")
-onready var background : Sprite =          get_node("../Player/Background")
-onready var spawn      : Position2D =      get_node("../Spawn")
-onready var player     : KinematicBody2D = get_node("../Player")
+onready var background : Sprite =          get_node("../../Player/Background")
+onready var spawn      : Position2D =      get_node("../../Spawn")
+onready var player     : KinematicBody2D = get_node("../../Player")
 
 const TRANS = Tween.TRANS_LINEAR
 const EASE  = Tween.EASE_IN_OUT
@@ -18,8 +18,8 @@ func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		spawn.position = position
 		player._on_checkpoint()
-		get_node('..').active = switch
-		get_node('../TileMap').update_tiles()
+		get_node('../..').active = switch
+		get_node('../../TileMap').update_tiles()
 		tween.interpolate_property(background, "modulate", background.modulate, color, duration, TRANS, EASE)
 		tween.start()
 		emit_signal("checkpoint")
